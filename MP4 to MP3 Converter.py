526d5a35aecc0d634f1b2eb7cf9419df22daf0e3
Brianveloper <108412466+Brianveloper@users.noreply.github.com>
2023-11-08 18:35:51 +0300

    Update MP4 to MP3 Converter.py
    Improved GUI

--- MP4 to MP3 Converter.py
+++ MP4 to MP3 Converter.py
@@ -1,47 +1,55 @@
-# ==== Importing all the necessary libraries
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
 import os
 from PIL import ImageTk
 import moviepy.editor as mp
 
-# ==== creating main class
 class VideoAudioConverter:
-    # ==== creating gui window
     def __init__(self, root):
         self.root = root
-        self.root.title("MP4 to MP3 Converter by BrianVeloper")
-        self.root.geometry('1280x720')
-        self.bg = ImageTk.PhotoImage(file="bg_image.jpg")
-        Label(self.root, image=self.bg).place(x=0, y=0)
+        self.root.title("MP4 to MP3 Converter by YobraVeloper")
+        self.root.geometry('600x400')
+        self.root.configure(bg='white')
 
-        Button(self.root,text="Browse Files",font=("times new roman", 15),command=self.browse).place(x=40, y=630)
+        # Styling
+        self.custom_font = ('Helvetica', 12)
 
-    # ==== browse data from system
+        # Background Image
+        self.bg = ImageTk.PhotoImage(file='C:\Jects\Misc/bg_image.jpg')
+        Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
+
+        # Labels and Instructions
+        Label(self.root, text="MP4 to MP3 Converter", font=("Arial", 20), bg='white').place(x=150, y=20)
+        Label(self.root, text="Select an MP4 video file to convert to MP3:", font=self.custom_font, bg='white').place(x=20, y=70)
+
+        # Browse Button
+        browse_button = Button(self.root, text="Browse Files", font=self.custom_font, command=self.browse)
+        browse_button.place(x=20, y=100)
+
+        # Output File Label
+        self.output_label = Label(self.root, text='', font=self.custom_font, bg="white")
+        self.output_label.place(x=20, y=150)
+
     def browse(self):
         self.file_name = filedialog.askopenfilename(title="Select a File", filetypes=(("Video files", "*.mp4*"),))
+        if self.file_name:
+            self.output_label.config(text=f"Selected File: {os.path.basename(self.file_name)}")
+            self.convert(os.path.basename(self.file_name))
 
-        Label(self.root, text=os.path.basename(self.file_name), font=("times new roman", 20), bg="blue").place(x=200, y=630)
+    def convert(self, filename):
+        try:
+            clip = mp.VideoFileClip(filename)
+            audio_filename = f"{filename[:-4]}.mp3"
+            clip.audio.write_audiofile(audio_filename)
+            messagebox.showinfo("Conversion Complete", f"File '{os.path.basename(filename)}' converted to '{os.path.basename(audio_filename)}'")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
-        Label(self.root, text='Processing...', font=("times new roman", 30)).place(x=600, y=630)
-        self.convert(os.path.basename(self.file_name))
-
-        Label(self.root, text='Completed!!', font=("times new roman", 30)).place(x=1000, y=630)
-
-    # ==== convert video to audio
-    def convert(self, path):
-        clip = mp.VideoFileClip(r'{}'.format(path))
-        clip.audio.write_audiofile(r'{}mp3'.format(path[:-3]))
-
-# ==== creating main function
 def main():
-    # ==== create tkinter window
     root = Tk()
-    # === creating object for class VideoAudioConverter
     obj = VideoAudioConverter(root)
-    # ==== start the gui
     root.mainloop()
 
 if __name__ == "__main__":
-    # ==== calling main function
     main()
